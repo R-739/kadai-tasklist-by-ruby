8092cb8c67f040bf1e3e20b7b3c5f5f7d0f6846f
R-739 <lchqq11003@yahoo.co.jp>
2020-08-10 13:09:27 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,14 @@
 class TasksController < ApplicationController
-    before_action :set_task, only:[:show, :edit, :update, :destroy]
     before_action :require_user_logged_in
-    
-    
+    before_action :correct_user, only: [:destroy,:show,:edit,:update]
     def index
-        @tasks = current_user.tasks
+        if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+        end
     end
+            
+
 
     def show
     end
@@ -15,7 +18,7 @@ class TasksController < ApplicationController
     end
 
     def create
-        @task = current_user.tasks.new(task_params)
+        @task = current_user.tasks.build(task_params)
         if @task.save
             flash[:success] = 'タスクが投稿されました。'
             redirect_to root_url
@@ -31,28 +34,26 @@ class TasksController < ApplicationController
     def update
         if @task.update(task_params)
             flash[:success] = 'タスクは正常に更新されました'
-            redirect_to @task
+            redirect_to root_url
         else
             flash.now[:danger] = 'タスクは更新されませんでした'
             render :edit
         end
     end
+    
 
     def destroy
         @task.destroy
         flash[:success] = 'タスクは正常に削除されました。'
-        redirect_to tasks_url
+        redirect_back(fallback_location: root_path)
     end
     
     private 
     
-    def set_task
-        @task = current_user.tasks.find(params[:id])
-    end
-    
     def task_params
         params.require(:task).permit(:content, :status)
     end
+    
     def correct_user
         @task = current_user.tasks.find_by(id: params[:id])
         unless @task
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-    before_action :require_user_logged_in, only: [:index, :show]
-  def show
-  end
   def new
       @user = User.new
   end
@@ -11,7 +8,7 @@ class UsersController < ApplicationController
       
       if @user.save
           flash[:success] = 'ユーザを登録しました。'
-          redirect_to @user
+          redirect_to root_url
       else 
             flash.now[:danger] = 'ユーザの登録に失敗しました。'
             render :new
